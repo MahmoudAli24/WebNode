532b8f2e2b72c4c691779a31facd61d81058ae6e
MahmoudAli24 <ememe50@yahoo.com>
2023-06-14 11:53:53 +0200

    dasf

---
+++ app.js
@@ -0,0 +1,12 @@
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAtgGPX7O4T2DKc7-wIfXVcKSuScf1sfcU",
+//   authDomain: "nodemcu-b67e2.firebaseapp.com",
+//   databaseURL: "https://nodemcu-b67e2-default-rtdb.firebaseio.com",
+//   projectId: "nodemcu-b67e2",
+//   storageBucket: "nodemcu-b67e2.appspot.com",
+//   messagingSenderId: "713380772294",
+//   appId: "1:713380772294:web:6c212ff81d15a5af491ac7",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
